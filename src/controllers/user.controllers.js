2776f862031a68427c0d8a3f62b671d91e500fe4
PushpamSingh <pushpamsingh204@gmail.com>
2025-06-21 08:44:13 +0530

    add api endPointds for registeruser, Login,logout,updateuserDetails, getcurrentUser

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,7 +2,9 @@ import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadCloudinary } from "../utils/uploadoncloudinary.js";
+import { deleteCloudinary } from "../utils/deletefromcloudiary.js";
 import { User } from "../models/user.models.js";
+import { isValidObjectId } from "mongoose";
 
 const generateAccessandRefreshToken=async(userID)=>{
     try {
@@ -133,3 +135,175 @@ const login=asyncHandler(async(req,res)=>{
         })
     }
 })
+
+const logout=asyncHandler(async(req,res)=>{
+    try {
+        //!find the user and set refreshToken null
+        //!return response in cookie
+
+        await User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                $set:{
+                    refreshToken:null
+                }
+            },
+            {
+                new:true
+            }
+        )
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+        return res
+        .status(200)
+        .cookie("accessToken",options)
+        .cookie("refreshToken",options)
+        .json(
+            new ApiResponse(
+                300,
+                {},
+                "user log out successfuly"
+            )
+        )
+    } catch (error) {
+        res.status(500).json({
+            message:`${error}`
+        })
+    }
+})
+
+const getcurrentuser=asyncHandler(async(req,res)=>{
+    try {
+        return res.status(200).json(
+            new ApiResponse(200,req.user,"current user fetched successfuly")
+        )
+    } catch (error) {
+        res.status(500).json(
+            {
+                message:`${error}`
+            }
+        )
+    }
+})
+
+const changePassword=asyncHandler(async(req,res)=>{
+    try {
+        //!get the data from req.body 
+        //!validate both the fields
+        //!check the user is exist or not
+        //!check oldPassword is matches or not
+        //!set new password
+        const {oldPassword,newPassword}=req.body;
+        const userID=req.user?._id;
+        if(!(oldPassword || newPassword)){
+            throw new ApiError(400,"Both the fields are required")
+        }
+        if(!isValidObjectId(userID)){
+            throw new ApiError(409,"User unAuthorized or invalid userID")
+        }
+        const user=await User.findById(userID);
+        if(!user){
+            throw new ApiError(404,"User not found")
+        }
+        const isPasswordMatch=await user.comparePassword(oldPassword);
+        if(!isPasswordMatch){
+            throw new ApiError(400,"Old Password is not matched")
+        }
+
+        const updatePassword=await User.findByIdAndUpdate(
+            userID,
+            {
+                $set:{
+                    password:newPassword
+                }
+            },
+            {
+                new:true
+            }
+        )
+        if(!updatePassword){
+            throw new ApiError(500,"Failed to update password")
+        }
+
+        return res.status(200).json(
+            new ApiResponse(200,updatePassword,"Password changed successfuly")
+        )
+    } catch (error) {
+          res.status(500).json(
+            {
+                message:`${error}`
+            }
+        )
+    }
+})
+
+const updateUserDetailes=asyncHandler(async(req,res)=>{
+    try {
+        //!get username email from req.body
+        //!validate both the field
+        //!get the avatar local path from req.file?.avatar[0].path
+        //!delete oldavatar from and cloudinary and upload new one
+        //!create a database call for user existance and update the detailes
+        //! return the response
+        const {username,email}=req.body;
+        const userID=req.user?._id;
+        
+        if(!(username || email)){
+            throw new ApiError(400,"username or email is required")
+        }
+
+        if(!isValidObjectId(userID)){
+            throw new ApiError(401,"Invalid userID or unAuthorized user")
+        }
+
+        const avatarLocalPath=req.file?.avatar[0]?.path;
+        if(!avatarLocalPath){
+            throw new ApiError(404,"avatar is required");
+        }
+        const user=await User.findById(userID);
+         if(!user){
+            throw new ApiError(404,"user not found");
+         }
+         const deleteAvatarCloudinary=await deleteCloudinary(user?.avatar);
+         if(!deleteAvatarCloudinary){
+            throw new ApiError(500,"cloudinary Error !! failed to delete avatar")
+         }
+         const uploadoncloudinary=await uploadCloudinary(avatarLocalPath);
+         if(!uploadoncloudinary){
+             throw new ApiError(500,"cloudinary Error !! failed to upload avatar")
+         }
+         const upadtedUser=await User.findByIdAndUpdate(
+            userID,
+            {
+                username,
+                email,
+                avatar:avatar?.url || ""
+            },
+            {
+                new:true
+            }
+         )
+
+         if(!upadtedUser){
+            throw new ApiError(503,"DataBase Error !! failed to update user Detailes")
+         }
+
+    } catch (error) {
+         res.status(500).json(
+            {
+                message:`${error}`
+            }
+        )
+    }
+})
+
+export {
+    registerUser,
+    login,
+    logout,
+    getcurrentuser,
+    changePassword,
+    updateUserDetailes
+}
