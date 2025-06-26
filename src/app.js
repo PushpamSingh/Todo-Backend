54b59ff14b7ec1956fdd67f5d6d99aa8c738c0b3
PushpamSingh <pushpamsingh204@gmail.com>
2025-06-26 11:10:45 +0530

    Fix typo in trust proxy setting in app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(
         }
     )
 )
-app.use('trust proxy',1) 
+app.set('trust proxy',1) 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({limit:"16kb",extended:true}))
 app.use(express.static("public"))
